8489e23b9139a39da380688398835eaac5820a76
Muhammad <mf9555759@gmail.com>
2025-12-01 14:22:50 +0800

    layouting

---
+++ Next JS/fundamental-nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="main-heading">
+      <h1>Selamat datang di Nextjs News</h1>
+      <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit reiciendis, ipsa unde labore porro facilis ut nulla, nemo sapiente blanditiis nostrum, magnam quo doloremque natus reprehenderit consectetur. Nulla, aliquam tempora?</p>
+    </div>
+  );
+}
